642d138f8c7cf911af98f2ff4b467753c97db44b
Adam Kowalski <adam.kowalski.work@gmail.com>
2024-03-18 22:28:07 -0700

    dark mode

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import { tokenize } from './tokenizer'
 import { parse } from './parser'
 
+
 document.addEventListener('DOMContentLoaded', () => {
     const code = document.getElementById('code') as HTMLTextAreaElement
     const output = document.getElementById('output') as HTMLPreElement
-    code.addEventListener('input', () => {
-        console.log("input changed")
-        let tokens = tokenize(code.value)
-        let ast = parse(tokens)
-        output.innerHTML = JSON.stringify(ast, null, 4)
-    })
+    const errorOutput = document.getElementById('error') as HTMLPreElement
+    function updateAst() {
+        try {
+            let tokens = tokenize(code.value)
+            let ast = parse(tokens)
+            output.innerHTML = JSON.stringify(ast, null, 4)
+            errorOutput.style.display = 'none'
+        } catch (error) {
+            console.warn(error)
+            errorOutput.innerHTML = error.toString()
+            errorOutput.style.display = 'block'
+        }
+    }
+    updateAst()
+    code.addEventListener('input', updateAst)
 })
 
-console.log("got here")
